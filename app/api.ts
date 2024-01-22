520ef2d4db6714f2d7855ba2e1f355b0d7fa220c
alaa-alshariff <alaa.alshariff@hotmail.com>
2024-01-22 01:38:45 -0500

    .

--- app/api.ts
+++ app/api.ts
@@ -1,6 +1,6 @@
 import { ITask } from "@/types/tasks";
 
-const baseUrl = "https://to-do-list-iota-indol.vercel.app";
+const baseUrl = "http://localhost:3001";
 
 export const getAllTodos = async (): Promise<ITask[]> => {
   const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
